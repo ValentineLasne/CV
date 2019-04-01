e80ffb38a8f2c50527d49e4e284c7fcabdf58d94
valentine lasne <valentinelasne@MacBook-Pro-de-valentine.local>
2019-04-01 12:04:12 +0200

    PHP

---
+++ contact.php
@@ -0,0 +1,5 @@
+<?php
+$sendEmailTo = 'Demo contact form <valentine.lasne@audencia.com>';
+$subject = 'Contact site Github';
+$okMessage = 'Merci pour votre message, je reviendrai vers vous rapidement';
+?>
