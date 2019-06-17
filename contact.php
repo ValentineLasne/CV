95aba4ca13ae196a8e8752a5c57030b3fb38980e
valentine lasne <valentinelasne@pc34.home>
2019-06-17 21:35:34 +0200

    contact3

--- contact.php
+++ contact.php
@@ -5,42 +5,14 @@ $message_non_envoye = "L'envoi du mail a échoué, veuillez réessayer SVP.";
 
 if (!isset($_POST['envoi']))
 {
-	// formulaire non envoyé
-	echo '<p>'.$message_erreur_formulaire.'</p>'."\n";
+	echo '<p>'.$message_non_envoye.'</p>'."\n";
 }
 else
 {
-	/*
-	 * cette fonction sert à nettoyer et enregistrer un texte
-	 */
-	function Rec($text)
-	{
-		$text = htmlspecialchars(trim($text), ENT_QUOTES);
-		if (1 === get_magic_quotes_gpc())
-		{
-			$text = stripslashes($text);
-		}
- 
-		$text = nl2br($text);
-		return $text;
-	};
- 
-	/*
-	 * Cette fonction sert à vérifier la syntaxe d'un email
-	 */
-	function IsEmail($email)
-	{
-		$value = preg_match('/^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!\.)){0,61}[a-zA-Z0-9_-]?\.)+[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!$)){0,61}[a-zA-Z0-9_]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/', $email);
-		return (($value === 0) || ($value === false)) ? false : true;
-	}
- 
-	// formulaire envoyé, on récupère tous les champs.
 	$email   = (isset($_POST['email']))   ? Rec($_POST['email'])   : '';
 	$message = (isset($_POST['message'])) ? Rec($_POST['message']) : '';
  
-	// On va vérifier les variables et l'email ...
-	$email = (IsEmail($email)) ? $email : ''; // soit l'email est vide si erroné, soit il vaut l'email entré
- 
+	
 	if ($email != '') && ($message != ''))
 	{
 		$headers  = 'MIME-Version: 1.0' . "\r\n";
@@ -50,17 +22,6 @@ else
 				'Content-Disposition: inline'. "\r\n" .
 				'Content-Transfer-Encoding: 7bit'." \r\n" .
 				'X-Mailer:PHP/'.phpversion();
- 
-		
-		// Remplacement de certains caractères spéciaux
-		$caracteres_speciaux     = array('&#039;', '&#8217;', '&quot;', '<br>', '<br />', '&lt;', '&gt;', '&amp;', '…',   '&rsquo;', '&lsquo;');
-		$caracteres_remplacement = array("'",      "'",        '"',      '',    '',       '<',    '>',    '&',     '...', '>>',      '<<'     );
- 
-		$objet = html_entity_decode($objet);
-		$objet = str_replace($caracteres_speciaux, $caracteres_remplacement, $objet);
- 
-		$message = html_entity_decode($message);
-		$message = str_replace($caracteres_speciaux, $caracteres_remplacement, $message);
  
 		// Envoi du mail
 		$num_emails = 0;
@@ -83,7 +44,7 @@ else
 	else
 	{
 		// une des 3 variables (ou plus) est vide ...
-		echo '<p>'.$message_formulaire_invalide.' <a href="contact.html">Retour au formulaire</a></p>'."\n";
+		echo '<p>'.$message_non_envoye.' <a href="index.html">Retour au formulaire</a></p>'."\n";
 	};
 }; // fin du if (!isset($_POST['envoi']))
 ?>
