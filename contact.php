f1e72d9d3a1def58063dfa3dd2befc0b5bf4fabc
valentine lasne <valentinelasne@pc34.home>
2019-06-17 21:11:07 +0200

    contact
    contact

--- contact.php
+++ contact.php
@@ -2,4 +2,11 @@
 $sendEmailTo = 'Demo contact form <valentine.lasne@audencia.com>';
 $subject = 'Contact site Github';
 $okMessage = 'Merci pour votre message, je reviendrai vers vous rapidement';
+$header =
+$header. = 'From :$'
+$message = 
+.$_POST['email'].
+.$_POST['message'].
+;
+mail("lasne.valentine@gmail.com", "Contact site Github", $message);
 ?>
